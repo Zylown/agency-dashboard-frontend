cd09d98da32178ef9316cc96f772cd13712991d1
Sevastian Caballero <115429070+Zylown@users.noreply.github.com>
2026-03-10 00:05:31 -0500

    feat: initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "../styles/globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "XdFloww",
+  description: "CRM para agencias de marketing digital y onlyfans",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
